7b973d99c8e740613dd6e44a7a7b0b33963994f3
Wiktoria <wp@MacBook-Air-Wiktoria.local>
2026-03-02 12:22:17 +0100

    Initial commit: Flask app with Docker

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from datetime import datetime
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return {
+        "message": "Hello from DevOps project!",
+        "timestamp": datetime.utcnow().isoformat(),
+        "hostname": os.uname().nodename
+    }
+
+@app.route("/health")
+def health():
+    return {
+        "status": "OK"
+    }
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
